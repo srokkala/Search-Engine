c07a7ca8c16266b1cc3a77f015c419a81501189b
Sophie Engle <sjengle@cs.usfca.edu>
2019-10-16 16:18:56 -0700

    Resubmit for quick review.

--- Project/src/InvertedBuilder.java
+++ Project/src/InvertedBuilder.java
@@ -51,15 +51,18 @@ public class InvertedBuilder {
 	 */
 	public  void addPath(Path file) throws IOException {
 		Stemmer stemmer = new SnowballStemmer(DEFAULT);
-		System.out.println("this is the path " + file);
+		System.out.println("this is the path " + file); // TODO Remove
 		try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);) {
 			String line = reader.readLine();
 			int i = 0;
+			// TODO String location = file.toString(); and reuse this value in your add
+			
 			while (line != null) {
 
 				String[] lines = TextParser.parse(line);
 
-				for (String elems : lines) {
+				for (String elems : lines) { // TODO refactor elems
+					// TODO String element = stemmer.stem(elems).toString();
 					String element = (String) stemmer.stem(elems.toString());
 					this.invertedIndex.add(element, file.toString(), ++i);
 				}
@@ -68,9 +71,22 @@ public class InvertedBuilder {
 
 		}
 
-		catch (Exception e) {
+		catch (Exception e) { // TODO Remove catch block
 			System.out.println("Something went wrong");
 		}
 
 	}
+	
+	/*
+	 * TODO For project 3 it helps to have a static and non-static option
+	 * for addPath.
+public static void addPath(Path file, InvertedIndex index) throws IOException {
+	current implementation of addPath
+}
+
+public void addPath(Path file) throws IOException {
+	addPath(file, this.invertedIndex);
+}
+	 *
+	 */
 }
